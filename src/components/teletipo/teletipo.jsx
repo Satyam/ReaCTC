ce37993a05eb2a8ca1fc94133725e3572a1b71f0
Satyam <satyam@satyam.com.ar>
2015-06-12 21:08:01 +0200

    fixed wrong formatting from beautifier

--- src/components/teletipo/teletipo.jsx
+++ src/components/teletipo/teletipo.jsx
@@ -3,6 +3,6 @@ require('./teletipo.less');
 
 export default class Teletipo extends React.Component {
 	render() {
-		return (< div className = "teletipo" > --Teletipo-- < /div>);
+		return (<div className="teletipo"> --Teletipo-- </div>);
 	}
 }
