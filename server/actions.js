9691dc5ef273cbf7a75800e07de094453fc13b40
Satyam <satyam@satyam.com.ar>
2015-06-14 22:44:56 +0200

    Added a primitive means to keep track of client machines and provide them with unique ids.

--- server/actions.js
+++ server/actions.js
@@ -31,17 +31,52 @@ module.exports = function (app, db) {
 		);
 	});
 
-	return Q.ninvoke(
-		db,
-		'run',
-		'create table if not exists actions (' +
-		'	id integer primary key,' +
-		'	timestamp text default CURRENT_TIMESTAMP,' +
-		'	sector text,' +
-		'   celda text,' +
-		'	action text,' +
-		'	data Text' +
-		');',
-		[]
+	app.get('/action/id', function (req, res) {
+		console.log('get id', req.ip, req.ips, req.get('user-agent'));
+		db.run(
+			'insert into terminalIds (ip, ua) values (?, ?)',
+			[
+				req.ip,
+				req.get('user-agent')
+			],
+			function (err) {
+				if (err) {
+					res.status(500).send(err);
+				}
+				res.send({
+					id: this.lastID,
+					changes: this.changes
+				});
+			}
+		);
+	});
+
+	return Q.all(
+		[
+			Q.ninvoke(
+				db,
+				'run',
+				'create table if not exists actions (' +
+				'	id integer primary key,' +
+				'	timestamp text default CURRENT_TIMESTAMP,' +
+				'	sector text,' +
+				'   celda text,' +
+				'	action text,' +
+				'	data Text' +
+				');',
+				[]
+			),
+			Q.ninvoke(
+				db,
+				'run',
+				'create table if not exists terminalIds (' +
+				'   id integer primary key,' +
+				'	timestamp text default CURRENT_TIMESTAMP,' +
+				'   ip text,' +
+				'   ua text' +
+				');',
+				[]
+			)
+		]
 	);
 };
--- src/stores/db.js
+++ src/stores/db.js
@@ -3,11 +3,29 @@ var Reflux = require('reflux');
 import http from '../common/http.js';
 import actions from '../actions.js';
 import path from 'path';
+import localConfigStore from './localConfig.js';
 
 // import _ from 'lodash';
 
+var terminalID;
+
 export default Reflux.createStore({
 	listenables: actions,
+	init: function() {
+        this.listenTo(localConfigStore, this.onLocalConfigChange);
+		terminalID = localConfigStore.getInitialState().terminalID;
+		if (!terminalID) {
+			http.get('/action/id').then(response => {
+				console.log('terminalID', response.body.id);
+				actions.setLocalConfig('terminalID',  response.body.id);
+			});
+		}
+	},
+	onLocalConfigChange: function (data) {
+		terminalID =  data.terminalID;
+		console.log('read terminalID', terminalID);
+	},
+
 	onCambio: function (data) {
 		http.post(path.join('/action', data.nombreSector, data.coords, 'cambio'), {desviado: data.desviado});
 	},
