74eac623d56f29bb54749adf71f22083be41e3c5
Satyam <satyam@satyam.com.ar>
2017-05-06 17:40:27 +0200

    Switched to morgan for server-side logging and dropped old code

--- webServer/server.js
+++ webServer/server.js
@@ -2,7 +2,7 @@ import http from 'http';
 import { join } from 'path';
 import express, { Router as createRouter } from 'express';
 import { MongoClient } from 'mongodb';
-import dbg from 'debug';
+import morgan from 'morgan';
 
 import bodyParser from 'body-parser';
 import denodeify from 'denodeify';
@@ -18,30 +18,12 @@ const listen = denodeify(server.listen.bind(server));
 const close = denodeify(server.close.bind(server));
 
 const dataRouter = createRouter();
-dbg.enable('CTC:server/server');
-const debug = dbg('CTC:server/server');
 
-app.use(REST_API_PATH, bodyParser.json(), /* showRequest, */ dataRouter);
+app.use(morgan('dev'));
+
+app.use(REST_API_PATH, bodyParser.json(), dataRouter);
 
 app.use('/bundles', express.static(absPath('bundles')));
-// app.get(join(REST_API_PATH, 'sectores'), (req, res) => {
-//   res.json({
-//     list: [
-//       {
-//         idSector: 'constitucion',
-//         descrCorta: 'Constitución',
-//       },
-//       {
-//         idSector: 'retiro',
-//         descrCorta: 'Retiro',
-//       },
-//     ],
-//   });
-// });
-//
-// app.get(join(REST_API_PATH, 'sectores/:idSector'), (req, res) => {
-//   res.sendFile(`${absPath('webServer/data')}/${req.params.idSector}.json`);
-// });
 app.use(express.static(absPath('webServer/public')));
 
 app.get('/kill', (req, res) => {
@@ -59,14 +41,10 @@ const handleRequest = actions => (req, res) => {
     options: req.query || {},
   };
 
-  debug('> %s %s %j', req.method, req.url, o);
   return []
     .concat(actions)
     .reduce((p, next) => p.then(next), Promise.resolve(o))
-    .then((reply) => {
-      debug('< %s %s %j', req.method, req.url, reply);
-      return res.json(reply);
-    })
+    .then(reply => res.json(reply))
     .catch((reason) => {
       res.status(reason.code || 500).send(reason.toString());
     });
