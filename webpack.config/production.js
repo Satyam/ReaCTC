b80f95357d58666f2fbe1499da63b7943896b35a
Satyam <satyam@satyam.com.ar>
2017-05-19 08:46:48 +0200

    Supressing the warnings is pointless

--- webpack.config/production.js
+++ webpack.config/production.js
@@ -1,12 +1,1 @@
-const webpack = require('webpack');
-
-module.exports = require('./common')('production').map((config) => {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-      },
-    })
-  );
-  return config;
-});
+module.exports = require('./common')('production');
