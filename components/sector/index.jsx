ca358db587d86c321d25369b48459b04ce06982a
Satyam <satyam@satyam.com.ar>
2017-06-26 20:37:14 +0200

    just a nicer way to identify an undefined cell

--- components/sector/index.jsx
+++ components/sector/index.jsx
@@ -6,11 +6,11 @@ import Celda from '_containers/celda';
 import { ANCHO_CELDA } from '../common';
 
 export default function SectorComponent({ ancho, alto, celdas }) {
-  return typeof ancho === 'undefined' // Any property would do just as well
-    ? <img alt="loading..." src="/loading.gif" />
-    : <svg viewBox={`0 0 ${ancho * ANCHO_CELDA} ${alto * ANCHO_CELDA}`}>
+  return ancho * alto > 0
+    ? <svg viewBox={`0 0 ${ancho * ANCHO_CELDA} ${alto * ANCHO_CELDA}`}>
       {celdas.map(idCelda => <Celda key={idCelda} idCelda={idCelda} />)}
-    </svg>;
+    </svg>
+    : <img alt="loading..." src="/loading.gif" />;
 }
 SectorComponent.propTypes = {
   ancho: PropTypes.number,
