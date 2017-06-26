2185db5e95ede71f8457197381b763e7644e3efb
Satyam <satyam@satyam.com.ar>
2017-06-26 20:37:58 +0200

    fix reference to ../common (the rest is prettier formatting)

--- components/celda/triple.jsx
+++ components/celda/triple.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Tramo from './tramo';
-import { DIR } from './common';
+import { DIR } from '../common';
 
 export default function Cambio({ celda }) {
-  return (<g>
-    <Tramo dest={celda.punta.dir} />
-    <Tramo dest={celda.centro.dir} estilo={celda.posicion ? 'tramo-muerto' : 'tramo-normal'} />
-    <Tramo dest={celda.izq.dir} estilo={celda.posicion !== -1 ? 'tramo-muerto' : 'tramo-normal'} />
-    <Tramo dest={celda.der.dir} estilo={celda.posicion !== 1 ? 'tramo-muerto' : 'tramo-normal'} />
-  </g>);
+  return (
+    <g>
+      <Tramo dest={celda.punta.dir} />
+      <Tramo dest={celda.centro.dir} estilo={celda.posicion ? 'tramo-muerto' : 'tramo-normal'} />
+      <Tramo
+        dest={celda.izq.dir}
+        estilo={celda.posicion !== -1 ? 'tramo-muerto' : 'tramo-normal'}
+      />
+      <Tramo dest={celda.der.dir} estilo={celda.posicion !== 1 ? 'tramo-muerto' : 'tramo-normal'} />
+    </g>
+  );
 }
 
 Cambio.propTypes = {
