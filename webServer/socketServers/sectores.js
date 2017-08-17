0aede096b0c39754903b6769b6e582f4b7cd70b4
Satyam <satyam@satyam.com.ar>
2017-08-17 18:09:31 +0200

    works both via rest and webSockets

--- webServer/socketServers/sectores.js
+++ webServer/socketServers/sectores.js
@@ -1,33 +1,27 @@
-import { normalize } from 'normalizr';
+import { LIST_SECTORES, GET_SECTOR, DELETE_SECTOR, ADD_SECTOR } from '_store/sectores/constants';
 
-import sectorSchema from '_utils/sectorSchema';
-import { LIST_SECTORES, GET_SECTOR, DELETE_SECTOR } from '_store/sectores/constants';
-
-let collection;
-
-export function init(db) {
-  collection = db.collection('sectores');
-  return Promise.resolve();
-}
+import * as sectores from '../dbOperations/sectores';
 
 export function listSectores() {
-  return collection.find({}, { idSector: 1, descrCorta: 1, descr: 1, _id: 0 }).toArray();
+  return sectores.listSectores();
 }
 
 export function getSector({ idSector }) {
-  return collection
-    .find({ _id: idSector })
-    .next()
-    .then(response => normalize(response, sectorSchema));
+  return sectores.getSector(idSector);
 }
 
 export function deleteSectores({ idSectores }) {
-  return collection.deleteMany({ idSector: { $in: idSectores } });
+  return sectores.deleteSectores(idSectores);
+}
+
+export function addSector(sector) {
+  return sectores.addSector(sector);
 }
 
 export default db =>
-  init(db).then(() => ({
+  sectores.init(db).then(() => ({
     [LIST_SECTORES]: listSectores,
     [GET_SECTOR]: getSector,
     [DELETE_SECTOR]: deleteSectores,
+    [ADD_SECTOR]: addSector,
   }));
